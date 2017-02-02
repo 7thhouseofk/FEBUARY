2bceb5ff0bbd8cc2548808b0b4c7ab31a7dbd531
game110pm <game110pm@605-110-STU10>
2017-02-02 13:07:13 -0700

    lyrics

---
+++ main.2/main.2/jay.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int input;
+	cout << "what song the want to play" << endl;
+	cin >> input;
+	if (input == 1)
+		cout << "1.wont back down from eminem" << endl;
+	if (input == 2)
+		cout << "see about you from david banner" << endl;
+	if (input == 3)
+		cout << "make it hurt from busta rhymes" << endl;
+
+}
