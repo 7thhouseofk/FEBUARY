5dad5610ac05bc3d140258969d029fc80ee389b0
game110pm <game110pm@605-110-STU10>
2017-02-08 12:45:32 -0700

    height

---
+++ pro.1/pro.1/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	double num1;
+	double num2;
+	double sum;
+	cout << "the heigth and width" << endl;
+	cin >> num1;
+	cin >> num2;
+	sum = 3.14*num1*num1*num2 / 3;
+	cout << "The area is " << sum << endl;
+	
+
+}
