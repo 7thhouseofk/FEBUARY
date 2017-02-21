ab1b35908ec97c045cf05e1d99a845e2543ae98a
game110pm <game110pm@605-110-STU10>
2017-02-21 13:12:15 -0700

    how many cookies

---
+++ how many cookies/how many cookies/how many cookies.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char input;
+	cout << "chocolate(c),almens(a),oatmeal(o)" << endl;
+	
+	cin >> input;
+	switch (input) {
+	case 'c':
+		cout << "chacolate" << endl;
+	case 'a':
+		cout << "alment" << endl;
+	case 'o':
+		cout << "oatmeal" << endl;
+
+
+    
+
+	}
+}
+
